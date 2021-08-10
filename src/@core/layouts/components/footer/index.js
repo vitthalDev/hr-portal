3c849c0b0b5cc244a367cd353057323bab690b80
vitthalDev <tusharkarna05@gmail.com>
2021-08-10 12:05:54 +0530

    changed spinner and icon

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -6,15 +6,15 @@ const Footer = () => {
     <p className='clearfix mb-0'>
       <span className='float-md-left d-block d-md-inline-block mt-25'>
         COPYRIGHT © {new Date().getFullYear()}{' '}
-        <a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer'>
-          Pixinvent
+        <a href='#' target='_blank' rel='noopener noreferrer'>
+          eOxegen
         </a>
         <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
       </span>
-      <span className='float-md-right d-none d-md-block'>
+      {/* <span className='float-md-right d-none d-md-block'>
         Hand-crafted & Made with
         <Heart size={14} />
-      </span>
+      </span> */}
     </p>
   )
 }
